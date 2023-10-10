8d42bcb534841f0dbb27201105e7120b1889880b
haroldocurti <haroldocurti@gmail.com>
2023-10-10 10:48:55 -0300

    Tarefa 3 - Analisar uma lista de produtos e encontrar o mais caro, mais barato e a média de preço

--- src/productList.php
+++ src/productList.php
@@ -37,7 +37,43 @@ function decrementStock($id, $quantity)
         echo 'Items Removed from stock.' . PHP_EOL;
     }
 }
+function getHighestPrice($productList): int{
+    $highestPriceId = null;
+    foreach ($productList as $id => $item) {
+        if($highestPriceId == null){
+            $highestPriceId = $id;
+            continue;
+        }
+        if($productList[$highestPriceId]['price'] < $item['price']){
+            $highestPriceId = $id;
+        }
 
+    }
+    echo 'The most expensive product is ' . $productList[$highestPriceId]['name'] . ' which costs ' . $productList[$highestPriceId]['price'] . PHP_EOL;
+    return $highestPriceId;
+}
+function getLowestPrice($productList): int{
+    $lowestPriceId = null;
+    foreach ($productList as $id => $item) {
+        if($lowestPriceId == null){
+            $lowestPriceId = $id;
+            continue;
+        }
+        if($productList[$lowestPriceId]['price'] > $item['price']){
+            $lowestPriceId = $id;
+        }
+
+    }
+    echo 'The cheapest product is ' . $productList[$lowestPriceId]['name'] . ' which costs ' . $productList[$lowestPriceId]['price'] . PHP_EOL;
+    return $lowestPriceId;
+}
+function getAveragePrice($productList): float{
+    $totalSum = 0.00;
+    foreach ($productList as $id => $item) {
+       $totalSum += $item['price'];
+    }
+    return $totalSum / count($productList);
+}
 $productList = [
     100 => [
         'name' => 'Super Mario World',
@@ -122,3 +158,7 @@ incrementStock(101,12);
 listItem(101);
 decrementStock(101,20);
 listItem(101);
+getLowestPrice($productList);
+getHighestPrice($productList);
+$average = getAveragePrice($productList);
+echo 'The average price is ' . $average;
